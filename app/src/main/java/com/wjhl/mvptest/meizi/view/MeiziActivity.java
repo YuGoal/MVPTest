3275787ada077e60d3b73f2061b32a30a43788e2
YuGoal <1127caoyu@gmail.com>
2016-09-09 17:42:37 +0800

    数据请求成功

--- app/src/main/java/com/wjhl/mvptest/meizi/view/MeiziActivity.java
+++ app/src/main/java/com/wjhl/mvptest/meizi/view/MeiziActivity.java
@@ -3,8 +3,11 @@ package com.wjhl.mvptest.meizi.view;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.widget.Button;
 
@@ -42,6 +45,8 @@ public class MeiziActivity extends AppCompatActivity{
     private int page = 1;
     private List<String> url = new ArrayList<>();
     private MeiZiAdapter mAdapter;
+    private boolean isLoadingMore = false;
+    StaggeredGridLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +62,29 @@ public class MeiziActivity extends AppCompatActivity{
             }
         });
 
-        recycleview.setLayoutManager(new LinearLayoutManager(this));//这里用线性显示 类似于listview
+//        recycleview.setLayoutManager(new LinearLayoutManager(this));//这里用线性显示 类似于listview
+//        recycleview.setLayoutManager(new GridLayoutManager(this, 2));//这里用线性宫格显示 类似于grid view
+        layoutManager = new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL);
+        recycleview.setLayoutManager(layoutManager);//这里用线性宫格显示 类似于瀑布流
+
+        recycleview.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int lastVisibleItem = new LinearLayoutManager(MeiziActivity.this).findLastVisibleItemPosition();
+                int totalItemCount = layoutManager.getItemCount();
+                //lastVisibleItem >= totalItemCount - 4 表示剩下4个item自动加载，各位自由选择
+                // dy>0 表示向下滑动
+                if (lastVisibleItem >= totalItemCount - 2 && dy > 0) {
+                    if(isLoadingMore){
+                        Log.d(TAG,"ignore manually update!");
+                    } else{
+                        getMeizi(String.valueOf(++page));//这里多线程也要手动控制isLoadingMore
+                        isLoadingMore = false;
+                    }
+                }
+            }
+        });
 
     }
 
@@ -67,7 +94,7 @@ public class MeiziActivity extends AppCompatActivity{
         map.put("showapi_appid", "24037");
         map.put("showapi_sign", "76e3b44c58e841458ad832387acf37f9");
         map.put("type", "34");
-        map.put("num", "10");
+        map.put("num", "20");
         map.put("page", page);
         Retrofit retrofit = new Retrofit.Builder()
                 //这里建议：- Base URL: 总是以/结尾；- @Url: 不要以/开头
@@ -82,16 +109,13 @@ public class MeiziActivity extends AppCompatActivity{
                     String res = response.body().string();
                     JSONObject mJson = new JSONObject(res);
                     Log.d(TAG, "onResponse: "+res);
-
-                    url.add(0,mJson.getJSONObject("showapi_res_body").getJSONObject("3").getString("thumb"));
-                    Log.d(TAG, "onResponse: "+url.get(0).toString());
                     for(int i=0;i<10;i++){
-
+                        url.add(i,mJson.getJSONObject("showapi_res_body").getJSONObject(String.valueOf(i)).getString("thumb"));
                     }
                     mAdapter = new MeiZiAdapter(MeiziActivity.this,url);
                     recycleview.setAdapter(mAdapter);
                     mAdapter.notifyDataSetChanged();
-
+                    swipeLayout.setRefreshing(false);
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
